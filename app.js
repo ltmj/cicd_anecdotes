a72313efa46783bf54d6c167c86586c5cd4612c6
ltmj <lauri.jaakkonen@tuni.fi>
2022-11-02 22:12:08 +0200

    utilizing express now

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const server = require('json-server')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use('/home', server.router('db.json'))
+
+app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`server started on port: ${PORT}`)
+})
+
+app.use((req, res, next) => {
+    next()
+})
+
+app.use(express.static('build'))
